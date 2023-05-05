6f170daffca717835ac71e3d7016c541c1a6ed9b
SeyeongPark <lynn.yeong.102@gmail.com>
2023-05-05 12:48:05 -0400

    fix currency popup message issue

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -15,10 +15,10 @@ const Budget = () => {
 
     // set a new timeout to check the input value after 2 seconds
     if (e.target.value > budgetLimit) {
-      window.alert(`Please enter a value less than or equal to the budget limit(£ ${budgetLimit}).`);
+      window.alert(`Please enter a value less than or equal to the budget limit(${currency} ${budgetLimit}).`);
       setInputBudget('');
     } else if (e.target.value < totalExpenses) {
-      window.alert(`Please enter a value more than or equal to the budget limit(£ ${totalExpenses}).`);
+      window.alert(`Please enter a value more than or equal to the budget limit(${currency} ${totalExpenses}).`);
       setInputBudget('');
     } else {
       dispatch({
@@ -32,8 +32,8 @@ const Budget = () => {
     <div className='alert alert-secondary'>
       Budget: {currency} 
       <input type='number' 
-        min={totalExpenses} 
-        max={budgetLimit}
+        // min={totalExpenses} 
+        // max={budgetLimit}
         onChange={handleInputChange}
         value={inputBudget}
         defaultValue={totalExpenses}
